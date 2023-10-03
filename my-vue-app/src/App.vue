ea6045d9dd4fa9a21257c834719b9c3a979c888a
DESKTOP-G5I4U2Q\JW <neojw1505@gmail.com>
2023-10-03 19:12:21 +0800

    Setup Firebase DB and Created Supermarket APIs
    Created 6 API methods to retrieve data from Firebase via title, price, supermarket, and if product promotions exist. Each of this method support ascending and descending sorting.

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,7 +1,67 @@
-<script></script>
-
 <template>
-  <h1>Hello World</h1>
+  <div>
+    <h1>Hello World</h1>
+    <button @click="fetchProductsByPromo">Fetch Supermarket Data</button>
+      <!-- Display supermarket data here -->
+      <div :style="{display: 'flex'}" v-for='product in supermarketData' :key="product.product_link">
+        <div :style="{ width: '50%'} ">
+          <h1>Product Name: {{ product.product_title }}</h1>
+          <h2 :style="{color: 'green'}">Product Price: {{ product.product_price }}</h2>
+          <h2 :style="{color: 'red'}">Product Promo: {{ product.product_promo }}</h2>
+          <h2 :style="{color: 'blue'}">Product Supermarket: {{ product.supermarket_name }}</h2>
+        </div>
+        <div :style="{ width: '50%'}"> 
+          <img :src="product.product_img" alt="">
+        </div>
+      </div>
+    <br>
+  </div>
 </template>
+
+<script>
+import * as smAPI from './firebaseService'; // Import all functions from smAPI
+
+export default {
+  name: 'YourComponentName',
+  data() {
+    return {
+      title: 'pork', // this value should be '' then v-model to input text 
+      supermarketData: [],
+      supermarketName: 'FairPrice', // Other options include: ColdStorage, FairPrice 
+      price: '5', // this value should be 0 by default (?) and then v-model to input text 
+      ascending: true // default value is true, sort in increasing price
+    };
+  },
+  methods: {
+    async fetchAllProducts(){
+      this.supermarketData = await smAPI.fetchAllProducts(); 
+    },
+    async fetchProductsBySupermarket(){
+      this.supermarketData = await smAPI.fetchProductsBySupermarket(this.supermarketName, false);
+    },
+    async fetchProductsByTitle(){
+      this.supermarketData = await smAPI.fetchProductsByTitle(this.title, false);
+    },
+    async fetchProductsByPrice(){
+      this.supermarketData = await smAPI.fetchProductsByPrice(this.price);
+    },
+    async fetchProductsByTitleAndPrice(){
+      this.supermarketData = await smAPI.fetchProductsByTitleAndPrice(this.title, this.price);
+    },
+    async fetchProductsByPromo(){
+      this.supermarketData = await smAPI.fetchProductsByPromo();
+    },
+  },
+  // You can use the `onMounted` hook to fetch data when the component is mounted
+  // async mounted() {
+  //   try {
+  //     this.supermarketData = await smAPI.fetchAllProducts(); // Call the function you need
+  //     // You can process or display the data here
+  //   } catch (error) {
+  //     console.error('Error fetching supermarket data:', error);
+  //   }
+  // },
+};
+</script>
 
 <style></style>
---
+++ my-vue-app/src/firebaseService.js
@@ -0,0 +1,256 @@
+// this file will contain methods to fetch data from firebase
+
+import { getDatabase, ref, get } from 'firebase/database';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './firebaseConfig'; // Import your Firebase configuration
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const database = getDatabase(firebaseApp);
+
+const baseURL = 'allSuperMarketsGroceries'
+
+// Fetch all products with optional ascending sorting by price
+export const fetchAllProducts = async (ascending = true) => {
+  const path = `/${baseURL}`; // Set the path to the root
+  const dataRef = ref(database, path);
+
+  try {
+    const snapshot = await get(dataRef);
+    if (snapshot.exists()) {
+      const allProducts = snapshot.val();
+      const allProductsArray = [];
+
+      // Iterate through the supermarkets and collect all products into one array
+      for (const supermarketName in allProducts) {
+        if (allProducts.hasOwnProperty(supermarketName)) {
+          allProductsArray.push(...allProducts[supermarketName]);
+        }
+      }
+
+      // Sort the products based on product_price in ascending or descending order
+      allProductsArray.sort((a, b) => {
+        const priceA = parseFloat(a.product_price.replace('$', ''));
+        const priceB = parseFloat(b.product_price.replace('$', ''));
+        const comparison = ascending ? priceA - priceB : priceB - priceA;
+        return comparison;
+      });
+
+      return allProductsArray;
+    } else {
+      console.log('No data available');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
+
+// Function to fetch all products from a specific supermarket sorted by price (ascending or descending)
+export const fetchProductsBySupermarket = async (supermarketName, ascending = true) => {
+  const path = `${baseURL}/${supermarketName}`;
+  const dataRef = ref(database, path);
+
+  try {
+    const snapshot = await get(dataRef);
+    if (snapshot.exists()) {
+      let products = Object.values(snapshot.val());
+
+      // Sort products by price
+      products = products.sort((a, b) => {
+        const priceA = parseFloat(a.product_price.match(/\$([\d.]+)/)[1]);
+        const priceB = parseFloat(b.product_price.match(/\$([\d.]+)/)[1]);
+        return ascending ? priceA - priceB : priceB - priceA;
+      });
+
+      return products;
+    } else {
+      console.log(`No data available for ${supermarketName}`);
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
+
+// Function to fetch products by product title with sorting by price
+export const fetchProductsByTitle = async (title, ascending = true) => {
+  const path = `/${baseURL}`;
+  const dataRef = ref(database, path);
+
+  try {
+    const snapshot = await get(dataRef);
+    if (snapshot.exists()) {
+      const allSupermarkets = snapshot.val();
+      const matchingProducts = [];
+
+      for (const supermarketName in allSupermarkets) {
+        if (allSupermarkets.hasOwnProperty(supermarketName)) {
+          const products = allSupermarkets[supermarketName];
+          for (const product of products) {
+            if (product.product_title.toLowerCase().includes(title.toLowerCase())) {
+              // Use a regular expression to extract the price in the format '$X.XX'
+              const priceMatch = product.product_price.match(/\$([\d.]+)/);
+              if (priceMatch) {
+                const productPrice = parseFloat(priceMatch[1]);
+                // Create a new object with the original data and an additional parsed_price field
+                const productWithParsedPrice = { ...product, parsed_price: productPrice };
+                matchingProducts.push(productWithParsedPrice);
+              }
+            }
+          }
+        }
+      }
+
+      // Sort matching products by parsed_price (price extracted from the format '$X.XX')
+      matchingProducts.sort((a, b) => {
+        return ascending ? a.parsed_price - b.parsed_price : b.parsed_price - a.parsed_price;
+      });
+
+      return matchingProducts;
+    } else {
+      console.log('No data available');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
+
+// Function to fetch products by product price with optional ascending sorting
+export const fetchProductsByPrice = async (price, ascending = true) => {
+  const path = `/${baseURL}`;
+  const dataRef = ref(database, path);
+
+  try {
+    const snapshot = await get(dataRef);
+    if (snapshot.exists()) {
+      const allProducts = snapshot.val();
+      const filteredProducts = [];
+
+      for (const supermarketName in allProducts) {
+        if (allProducts.hasOwnProperty(supermarketName)) {
+          const products = allProducts[supermarketName];
+          const matchingProducts = products.filter((product) => {
+            // Parse the product price and user input price to numbers
+            const productPrice = parseFloat(product.product_price.replace('$', ''));
+            const userInputPrice = parseFloat(price);
+            // Compare the parsed prices based on ascending or descending sorting
+            return productPrice <= userInputPrice;
+          });
+
+          filteredProducts.push(...matchingProducts);
+        }
+      }
+
+      // Sort the filtered products based on product_price in descending order
+      filteredProducts.sort((a, b) => {
+        const priceA = parseFloat(a.product_price.replace('$', ''));
+        const priceB = parseFloat(b.product_price.replace('$', ''));
+        return ascending ? priceA - priceB : priceB - priceA;
+      });
+
+      return filteredProducts;
+    } else {
+      console.log('No data available');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
+
+// Function to fetch products with promotions and optional sorting by price
+export const fetchProductsByPromo = async (ascending = true) => {
+  const path = `/${baseURL}`;
+  const dataRef = ref(database, path);
+
+  try {
+    const snapshot = await get(dataRef);
+    if (snapshot.exists()) {
+      const allProducts = snapshot.val();
+      const promotedProducts = [];
+
+      for (const supermarketName in allProducts) {
+        if (allProducts.hasOwnProperty(supermarketName)) {
+          const products = allProducts[supermarketName];
+          const matchingProducts = products.filter((product) =>
+            product.product_promo && product.product_promo !== 'No promotion available'
+          );
+
+          promotedProducts.push(...matchingProducts);
+        }
+      }
+
+      // Sort promoted products by price if specified
+      if (ascending) {
+        promotedProducts.sort((a, b) => {
+          const priceA = parseFloat(a.product_price.match(/\$([\d.]+)/)[1]);
+          const priceB = parseFloat(b.product_price.match(/\$([\d.]+)/)[1]);
+          return priceA - priceB;
+        });
+      } else {
+        promotedProducts.sort((a, b) => {
+          const priceA = parseFloat(a.product_price.match(/\$([\d.]+)/)[1]);
+          const priceB = parseFloat(b.product_price.match(/\$([\d.]+)/)[1]);
+          return priceB - priceA;
+        });
+      }
+
+      return promotedProducts;
+    } else {
+      console.log('No data available');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
+// Function to fetch products by title, price, and sorting order
+export const fetchProductsByTitleAndPrice = async (title, maxPrice, ascending = true) => {
+  const path = `/${baseURL}`; // Set the path to the root
+  const dataRef = ref(database, path);
+
+  try {
+    const snapshot = await get(dataRef);
+    if (snapshot.exists()) {
+      const allProducts = snapshot.val();
+      const matchingProducts = [];
+
+      // Iterate through the supermarkets and filter products by title and price
+      for (const supermarketName in allProducts) {
+        if (allProducts.hasOwnProperty(supermarketName)) {
+          const products = allProducts[supermarketName];
+
+          const filteredProducts = products.filter((product) =>
+            product.product_title.toLowerCase().includes(title.toLowerCase()) &&
+            parseFloat(product.product_price.replace('$', '')) <= maxPrice
+          );
+
+          matchingProducts.push(...filteredProducts);
+        }
+      }
+
+      // Sort the matching products based on sorting order
+      matchingProducts.sort((a, b) => {
+        const priceA = parseFloat(a.product_price.replace('$', ''));
+        const priceB = parseFloat(b.product_price.replace('$', ''));
+        const comparison = ascending ? priceA - priceB : priceB - priceA;
+        return comparison;
+      });
+
+      return matchingProducts;
+    } else {
+      console.log('No data available');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
+};
