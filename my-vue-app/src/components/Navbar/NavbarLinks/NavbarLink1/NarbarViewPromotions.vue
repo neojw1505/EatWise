9850157de8ede97e9aebb59ae197027793fc88de
wang-kaijie <99939212+wang-kaijie@users.noreply.github.com>
2023-10-05 20:04:07 +0800

    add in navigation bar and do routing

---
+++ my-vue-app/src/components/Navbar/NavbarLinks/NavbarLink1/NarbarViewPromotions.vue
@@ -0,0 +1,21 @@
+<template>
+  <li class="nav-item mx-2">
+    <img class="logo" src="../../images/view_promotion.png" alt="" type="button" />
+    <a class="nav-link py-0" href="#">View Promotions</a>
+  </li>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.logo{
+    width: 25px;
+    height: 25px;
+}
+li:hover{
+            background-color: #AED4B7;
+            border-radius: 25px;
+        }
+</style>
--- my-vue-app/src/router.js
+++ my-vue-app/src/router.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from './components/Home.vue';
-// import FindRecipes from './components/FindRecipes.vue';
+import FindRecipes from './components/FindRecipes.vue';
 
 const routes = [
     { path: '/', component: Home },
-    // { path: '/find-recipes', component: FindRecipes },
+    { path: '/find-recipes', component: FindRecipes },
     // { path: '/view-promos', component: ViewPromos },
     // { path: '/market', component: Market },
     // Add more routes as needed
