99fe1db4d771d73e5809cf45ec42c9983cd3534d
Zavier <zavier.yan.2022@scis.smu.edu.sg>
2023-10-06 00:25:39 +0800

    Created Carousel Component
    Contains some bugs.

--- my-vue-app/src/components/NewPromotions/NewPromotions.vue
+++ my-vue-app/src/components/NewPromotions/NewPromotions.vue
@@ -1,20 +1,17 @@
 <template>
-    <div>
-      <button @click="fetchProductsByPromo" class="btn btn-primary mb-3">Fetch Supermarket Data</button>
-      <!-- Display supermarket data here -->
-      <div class="container text-center mx-auto">
-        <div class="row justify-content-center">
-          <div class="owl-carousel">
-            <div class="card equal-card shadow mb-4" v-for="product in getRandomProducts()" :key="product.product_link">
+    <div v-if="this.supermarketData">
+        <div class="container text-center mx-auto">
+        <div class="owl-carousel">
+        <div class="card equal-card shadow mb-4" v-for="product in getRandomProducts()" :key="product.product_link">
               <div class="circle-overlay">
                 <div class="promo-text">
                   {{ product.product_promo }}
                 </div>
               </div>
-              <img class="card-img-top" :src="product.product_img" alt="Product Image" style="height: 300px; object-fit: cover;">
-              <div class="card-body d-flex flex-column">
+              <img class="card-img-top" :src="product.product_img" alt="Product Image" />
+              <div class="card-body">
                 <h5 class="card-title">{{ product.product_title }}</h5>
-                <div class="flex-grow-1 overflow-hidden"> <!-- Use flex-grow-1 to fill available space & overflow-hidden to hide overflow of text -->
+                <div>
                   <p class="card-text">
                     <strong>{{ product.product_price }}</strong> <br />
                     {{ product.supermarket_name }}
@@ -24,99 +21,96 @@
                   <a href="#" class="btn btn-primary">Go Somewhere</a>
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
-        <div class="text-center">
-          <a href="#" class="btn btn-primary">View More</a>
         </div>
       </div>
-    </div>
+      </div>
+      </div>
   </template>
   
-  
-  
-  
-
-<script>
-
-export default {
-  data() {
-    return {
-      supermarketData: [],
-      ascending: true, // default value is true, sort in increasing price
-    };
-  },
-  methods: {
-    getRandomProducts() {
-      const shuffledData = [...this.supermarketData].sort(() => Math.random() - 0.5);
-      // Return the first 10 items
-      return shuffledData.slice(0, 24);
+  <script>
+  export default {
+    mounted() {
+      this.fetchProductsByPromo(); // Call the method here
     },
-
-    async fetchProductsByPromo() {
-        this.supermarketData = await this.$smAPI.fetchProductsByPromo(false);
-        this.initOwlCarousel();
+    data() {
+      return {
+        supermarketData: [],
+        ascending: true, // default value is true, sort in increasing price
+      };
     },
-
-    initOwlCarousel() {
-        $(document).ready(function(){
-          $(".owl-carousel").owlCarousel(
-            {
-                items: 3,
-                autoPlay: true,
-                autoPlaySpeed: 2000,
-                autoPlayTimeout: 2000,
-                autoplayHoverPause: true,
-                center: true
-            }
-          );
-        });
-    },
-
-    async fetchProductsByPromoAndSupermarket() {
-      this.supermarketData =
-        await this.$smAPI.fetchProductsByPromoAndSupermarket(
-          this.supermarketName,
-          false
+    methods: {
+      getRandomProducts() {
+        const shuffledData = [...this.supermarketData].sort(
+          () => Math.random() - 0.5
         );
+        // Return the first 10 items
+        return shuffledData.slice(0, 24);
+      },
+  
+      async fetchProductsByPromo() {
+        this.supermarketData = await this.$smAPI.fetchProductsByPromo(false);
+        this.getRandomProducts();
+        this.initOwlCarousel();
+      },
+
+      initOwlCarousel() {
+        // Use this.$nextTick to ensure the DOM is ready
+        this.$nextTick(() => {
+        $(".owl-carousel").owlCarousel({
+            items: 3,
+            autoplay: 2000, // Set the interval in milliseconds
+            autoplaySpeed: 2000, // Use "autoplaySpeed" instead of "autoPlaySpeed"
+            autoplayTimeout: 2000, // Use "autoplayTimeout" instead of "autoPlayTimeout"
+            autoplayHoverPause: true,
+            center: true,
+        });
+    });
     },
-  },
 
-};
-</script>
-
-<style scoped>
+      async fetchProductsByPromoAndSupermarket() {
+        this.supermarketData =
+          await this.$smAPI.fetchProductsByPromoAndSupermarket(
+            this.supermarketName,
+            false
+          );
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
   /* Add hover effect to the card */
   .container {
     background-color: #1c4946; /* Pastel green color */
     padding: 1.5%; /* Add some padding for spacing */
-}
+  }
   .card:hover {
     background-color: #f0f0f0; /* You can choose your desired highlight color */
     transition: background-color 0.3s ease; /* Smooth transition effect */
-}
+  }
+  .card-img-top {
+    height: 300px; 
+    object-fit: cover
+  }
 
   .equal-card {
-  width: 100%; 
-  max-width: 20rem; /* Set a maximum width if needed */
-  height: 30rem;
-  border: none;
-  border-radius: 10px;
-  transition: transform 0.2s;
-  margin-right: 1rem; /* Add right margin for spacing between cards */
-}
-
-.equal-card:last-child {
-  margin-right: 0; /* Remove right margin from the last card */
-}
-
-.circle-overlay {
+    width: 100%;
+    max-width: 20rem; /* Set a maximum width if needed */
+    height: 30rem;
+    border: none;
+    border-radius: 10px;
+    transition: transform 0.2s;
+    margin-right: 1rem; /* Add right margin for spacing between cards */
+  }
+  
+  .equal-card:last-child {
+    margin-right: 0; /* Remove right margin from the last card */
+  }
+  
+  .circle-overlay {
   position: absolute;
   top: 10px;
   right: 10px;
-  width: 6rem;
-  height: 6rem;
   background-color: red;
   border-radius: 50%;
   display: flex;
@@ -125,27 +119,34 @@ export default {
   color: white;
   font-weight: bold;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+  width: 6rem; /* Set a fixed width for the circle */
+  height: 6rem; /* Set a fixed height for the circle */
+  padding: 0; /* Remove padding */
   animation: circleAnimation 3s infinite;
-}
-
-@keyframes circleAnimation {
-  0% {
-    background-color: red;
-    transform: scale(1);
-  }
-  50% {
-    background-color: #ff5733;
-    transform: scale(1.1);
-  }
-  100% {
-    background-color: red;
-    transform: scale(1);
-  }
+  overflow: hidden; /* Hide any text that overflows the circle */
 }
 
 .promo-text {
   font-size: 16px;
-  max-width: 100%;
+  max-width: 100%; /* Allow text to take full width of the circle */
   text-align: center;
+  white-space: normal; /* Allow text to wrap within the circle */
+  padding: 5px; /* Add padding inside the circle to separate text from the edge */
 }
-</style>
+  
+  @keyframes circleAnimation {
+    0% {
+      background-color: red;
+      transform: scale(1);
+    }
+    50% {
+      background-color: #ff5733;
+      transform: scale(1.1);
+    }
+    100% {
+      background-color: red;
+      transform: scale(1);
+    }
+  }
+  
+  </style>
