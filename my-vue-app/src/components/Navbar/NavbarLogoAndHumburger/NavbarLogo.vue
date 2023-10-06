94684cf641cfb4230ffe5da27acab3a26dca96b4
wang-kaijie <99939212+wang-kaijie@users.noreply.github.com>
2023-10-06 12:00:43 +0800

    make navbar light up( change background colour) when it is in certain page

--- my-vue-app/src/components/Navbar/NavbarLogoAndHumburger/NavbarLogo.vue
+++ my-vue-app/src/components/Navbar/NavbarLogoAndHumburger/NavbarLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center mx-2">
-    <li style="list-style: none">
+    <li :class="this.$route.path==='/' ? 'highlightBackground' : ''" @click="highlighLink" style="list-style: none">
       <router-link class="navbar-brand p-0 m-0" to="/">
         <img class="logo" src="../images/companyLogo.png" alt="" type="button" />
         <div>EATWISE</div>
@@ -16,6 +16,10 @@ export default {
 </script>
 
 <style scoped>
+.highlightBackground{
+    background-color: #AED4B7;
+    border-radius: 25px;
+}
 .logo{
     width: 25px;
     height: 25px;
