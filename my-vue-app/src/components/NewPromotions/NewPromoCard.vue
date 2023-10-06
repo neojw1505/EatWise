b6f134c45c8b585b6271cfcccb0091bff974d7d9
Zavier <zavier.yan.2022@scis.smu.edu.sg>
2023-10-06 14:44:25 +0800

    Fixed Some Styling Issues

---
+++ my-vue-app/src/components/NewPromotions/NewPromoCard.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="card equal-card shadow mb-4">
+      <!-- Remove the circle-overlay div -->
+      <img class="card-img-top" :src="product.product_img" alt="Product Image" />
+      <!-- Add a rectangle-overlay div -->
+      <div class="rectangle-overlay">
+        <div class="promo-text">{{ product.product_promo }}</div>
+      </div>
+      <div class="card-content">
+        <h5 class="card-title">{{ product.product_title }}</h5>
+        <div>
+          <p class="card-text">
+            <strong>{{ product.product_price }}</strong> <br />
+            {{ product.supermarket_name }}
+          </p>
+        </div>
+      </div>
+      <div class="text-center card-button-container">
+        <a href="#" class="btn btn-primary">Go Somewhere</a>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      product: {
+        type: Object,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add hover effect to the card */
+  .card:hover {
+    background-color: #f0f0f0;
+    transition: background-color 0.3s ease;
+  }
+  .card-img-top {
+    height: 300px;
+    object-fit: cover;
+  }
+  .equal-card {
+    width: 100%;
+    max-width: 20rem;
+    height: 30rem;
+    border: none;
+    border-radius: 10px;
+    transition: transform 0.2s;
+    margin-right: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  .equal-card:last-child {
+    margin-right: 0;
+  }
+  /* Style the rectangle-overlay */
+  .rectangle-overlay {
+    background-color: rgba(255, 0, 0, 0.7);
+    color: white;
+    padding: 10px;
+    width: calc(100% - 20px); /* Take up the width of the card minus 20px for padding */
+    position: absolute;
+    top: 0;
+    left: 10px; /* Adjust left positioning to 10px */
+    right: 10px; /* Add right positioning to 10px */
+    border-radius: 15px; /* Rounded corners for the entire overlay */
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.3);
+    animation: overlayAnimation 2s alternate infinite; /* Add an animation to the overlay */
+}
+
+.promo-text {
+    font-size: 20px; /* Increase the font size for more prominence */
+    font-weight: bold; /* Make the promo text bold */
+    max-width: 100%;
+    white-space: normal;
+}
+
+@keyframes overlayAnimation {
+  0% {
+    background-color: red;
+    transform: scale(1);
+  }
+  50% {
+    background-color: #ff5733; /* Change color at 50% of the animation */
+    transform: scale(1.1);
+  }
+  100% {
+    background-color: red;
+    transform: scale(1);
+  }
+}
+  .card-content {
+    flex-grow: 1;
+  }
+  .card-button-container {
+    margin-top: auto;
+    padding-bottom: 20px; /* Adjust as needed */
+  }
+  </style>
+  
---
+++ my-vue-app/src/components/NewPromotions/NewPromoCarousel.vue
@@ -0,0 +1,94 @@
+<template>
+  <div v-if="supermarketData" class="smaller-container text-center mx-auto">
+    <div class="owl-carousel">
+      <new-promo-card
+        v-for="product in randomProducts"
+        :key="product.product_link"
+        :product="product"
+      />
+    </div>
+
+    <div class="text-center py-4">
+        <button type="button" class="btn btn-primary pill">View More</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import NewPromoCard from "./NewPromoCard.vue";
+export default {
+  components: {
+    NewPromoCard,
+  },
+  mounted() {
+    this.fetchProductsByPromo();
+  },
+  data() {
+    return {
+      supermarketData: [],
+      ascending: true,
+    };
+  },
+  computed: {
+    randomProducts() {
+      const shuffledData = [...this.supermarketData].sort(
+        () => Math.random() - 0.5
+      );
+      return shuffledData.slice(0, 24);
+    },
+  },
+  methods: {
+    async fetchProductsByPromo() {
+      this.supermarketData = await this.$smAPI.fetchProductsByPromo(false);
+      this.initOwlCarousel();
+    },
+    initOwlCarousel() {
+      this.$nextTick(() => {
+        $(".owl-carousel").owlCarousel({
+          items: 3,
+          autoPlay: 4000,
+          autoPlaySpeed: 3000,
+          autoPlayTimeout: 2000,
+          autoPlayHoverPause: true,
+          center: true,
+          responsiveClass: true,
+          responsive: {
+            0: {
+              items: 1,
+            },
+            480: {
+              items: 2,
+            },
+            768: {
+              items: 3,
+            },
+          },
+          dotsContainer: '.custom-dots',
+        });
+      });
+    },
+    async fetchProductsByPromoAndSupermarket() {
+      this.supermarketData =
+        await this.$smAPI.fetchProductsByPromoAndSupermarket(
+          this.supermarketName,
+          false
+        );
+    },
+  },
+};
+</script>
+
+<style>
+.smaller-container {
+  max-width: 80rem;
+  padding-inline-start: 5rem;
+  background-color:#007459
+}
+
+.custom-dots {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px; /* Adjust as needed */
+}
+</style>
