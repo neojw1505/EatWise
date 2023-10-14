05b15f07cc00f7b6a262d2b8ba84435e5dc8ebf3
Zavier <zavier.yan.2022@scis.smu.edu.sg>
2023-10-15 01:28:39 +0800

    Removed Unwated Files, Edited Meal Plan Component on Home Page
    - Removed Duplicated Meal Plan files
    - Added Save Recipe Button
    - Added Eaten/ Not Eaten Button
    - Edited Colours to final colours

---
+++ my-vue-app/src/components/Home.vue
@@ -0,0 +1,28 @@
+<!-- Home.vue -->
+<template>
+  <div>
+    <Navbar/>
+  <div class="home-container">
+    <div class="col-9">
+      <MealPlan/>
+      <NewPromoCarousel/>
+    </div>
+    <div class="col-3">
+      <div>
+        <h1>XP Level?</h1>
+        <h1>Random Food Fact?</h1>
+        <h1>Random Food Joke?</h1>
+        <h1>Calories Tracker?</h1>
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
+
+
+<style scoped>
+  .home-container {
+    /* background-color: #007459; */
+    display: flex;
+  }
+</style>
---
+++ my-vue-app/src/components/MealPlan/MealPlan.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="container-fluid border border-success">
+    <div class="row">
+      <div class="col">
+        <h1>Your Meal Plans</h1>
+      </div>
+    </div>
+    <div class="row d-flex pb-5">
+      <div class="d-flex">
+        <BreakfastRecipe
+          v-if="breakfastRecipe && breakfastNutrition"
+          :recipeData="breakfastRecipe"
+          :nutritionData="breakfastNutrition"
+          @refresh-recipe="refreshRecipe"
+          style="margin-right: 0.5rem"
+        />
+        <LunchRecipe
+          v-if="lunchRecipe && lunchNutrition"
+          :recipeData="lunchRecipe"
+          :nutritionData="lunchNutrition"
+          @refresh-recipe="refreshRecipe"
+          style="margin-right: 0.5rem"
+        />
+        <DinnerRecipe
+          v-if="dinnerRecipe && dinnerNutrition"
+          :recipeData="dinnerRecipe"
+          :nutritionData="dinnerNutrition"
+          @refresh-recipe="refreshRecipe"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  mounted() {
+    this.getBreakfastRecipe();
+    this.getLunchRecipe();
+    this.getDinnerRecipe();
+  },
+  data() {
+    return {
+      number: 1, // default 1 recipe
+
+      breakfastRecipe: null,
+      breakfastNutrition: null, // Add this
+      BreakfastRecipesCategory: [
+        "morning meal",
+        "brunch",
+        "breakfast",
+        "bread",
+        "soup",
+        "salad",
+        "side dish",
+      ],
+      lunchRecipe: null,
+      lunchNutrition: null, // Add this
+      LunchRecipesCategory: [
+        "lunch",
+        "main course",
+        "main dish",
+        "salad",
+        "side dish",
+      ],
+      dinnerRecipe: null,
+      dinnerNutrition: null, // Add this
+      DinnerRecipesCategory: [
+        "main course",
+        "main dish",
+        "dinner",
+        "dessert",
+        "salad",
+        "side dish",
+      ],
+    };
+  },
+  methods: {
+    async getBreakfastRecipe() {
+      this.breakfastRecipe = await this.$spoonAPI.getBreakfastRecipe(
+        this.BreakfastRecipesCategory,
+        this.number
+      );
+      this.breakfastNutrition = await this.$spoonAPI.getSelectedRecipeNutritions(this.breakfastRecipe.id);
+      console.log(this.breakfastRecipe);
+      console.log(this.breakfastNutrition);
+    },
+    async getLunchRecipe() {
+      this.lunchRecipe = await this.$spoonAPI.getLunchRecipe(
+        this.LunchRecipesCategory,
+        this.number
+      );
+      this.lunchNutrition = await this.$spoonAPI.getSelectedRecipeNutritions(this.lunchRecipe.id); 
+    },
+    async getDinnerRecipe() {
+      this.dinnerRecipe = await this.$spoonAPI.getDinnerRecipe(
+        this.DinnerRecipesCategory,
+        this.number
+      );
+      this.dinnerNutrition = await this.$spoonAPI.getSelectedRecipeNutritions(this.dinnerRecipe.id); 
+    },
+    async refreshRecipe(mealType){
+      if (mealType === 'breakfast') {
+        // Fetch a new breakfast recipe when the child requests a refresh for breakfast
+        await this.getBreakfastRecipe()
+      } else if (mealType === 'lunch') {
+        await this.getLunchRecipe()
+      } else if (mealType === 'dinner') {
+        await this.getDinnerRecipe()
+      }
+    }
+  },
+};
+</script>
+
+<style scoped>
+  /* h1 {
+    color: #FFF;
+  } */
+  .container-fluid {
+    background-color: #FFF;
+    padding: 50px;
+  }
+</style>
