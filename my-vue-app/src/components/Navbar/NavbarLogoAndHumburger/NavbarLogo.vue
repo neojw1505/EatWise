45198ff6242ec81f6f19334b377350c1faedbfda
Zavier <zavier.yan.2022@scis.smu.edu.sg>
2023-10-18 12:48:29 +0800

    Updated NavBar Colours

--- my-vue-app/src/components/Navbar/NavbarLogoAndHumburger/NavbarLogo.vue
+++ my-vue-app/src/components/Navbar/NavbarLogoAndHumburger/NavbarLogo.vue
@@ -3,8 +3,12 @@
     <li :class="this.$route.path==='/' ? 'highlightBackground' : ''" @click="highlighLink" style="list-style: none">
       <router-link class="navbar-brand p-0 m-0" to="/">
         <img class="logo" src="../images/companyLogo.png" alt="" type="button" />
+<<<<<<< Updated upstream
         <div>EATWISE</div>
       </router-link>
+=======
+        <div class="fw-semibold">EatWise</div>
+>>>>>>> Stashed changes
     </li>
   </div>
 </template>
@@ -17,7 +21,11 @@ export default {
 
 <style scoped>
 .highlightBackground{
+<<<<<<< Updated upstream
     background-color: #AED4B7;
+=======
+    background-color: #FFB18D;
+>>>>>>> Stashed changes
     border-radius: 25px;
 }
 .logo{
@@ -25,7 +33,11 @@ export default {
     height: 25px;
 }
 li:hover{
+<<<<<<< Updated upstream
             background-color: #AED4B7;
+=======
+            background-color: #FBE8A6;
+>>>>>>> Stashed changes
             border-radius: 25px;
         }
 </style>
