0afef11dea0d89132140f006a5e453548b3227f5
Zavier <zavier.yan.2022@scis.smu.edu.sg>
2023-10-29 01:35:32 +0800

    Added Sweet Alert, Standardised radio options styling

--- my-vue-app/src/components/Onboarding/OnboardingPages/OnboardingUserDetails.vue
+++ my-vue-app/src/components/Onboarding/OnboardingPages/OnboardingUserDetails.vue
@@ -2,40 +2,48 @@
   <div>
     <div class="container my-5">
       <div class="rounded-4 p-3 text-center" style="background-color: #FFB18D;">
-    <h1 class="fw-bold fs-4">TELL US MORE ABOUT YOURSELF</h1>
-  </div>
-  <div class="border rounded-4 p-3 shadow bg-white mt-3">
-    <h3 class="fw-bold fs-4 py-3">Basic Information</h3>
-    <div class="row">
-      <div class="col-12 col-sm-4 mb-3">
-        <input type="text" class="form-control" name="age" placeholder="Age" v-model="inputAge" required>
+        <h1 class="fw-bold fs-4">TELL US MORE ABOUT YOURSELF</h1>
       </div>
-      <div class="col-12 col-sm-4 mb-3">
-        <input type="text" class="form-control" name="weight" placeholder="Your Weight (kg)" v-model="inputWeight" required>
+      <div class="border rounded-4 p-3 shadow bg-white mt-3">
+        <h3 class="fw-bold fs-4 py-3">Basic Information</h3>
+        <div class="row">
+          <div class="col col-sm-4 mb-3">
+            <input type="text" class="form-control" name="age" placeholder="Age" v-model="inputAge" required>
+          </div>
+          <div class="col col-sm-4 mb-3">
+            <input type="text" class="form-control" name="weight" placeholder="Your Weight (kg)" v-model="inputWeight" required>
+          </div>
+          <div class="col col-sm-4 mb-3">
+            <input type="text" class="form-control" name="height" placeholder="Your Height (cm)" v-model="inputHeight" required>
+          </div>
+        </div>
       </div>
-      <div class="col-12 col-sm-4 mb-3">
-        <input type="text" class="form-control" name="height" placeholder="Your Height (cm)" v-model="inputHeight" required>
-      </div>
-    </div>
-</div>
     </div>
     <div class="mx-auto text-center my-5 rounded-4 col-6 py-2" style="background-color: #FFB18D;">
       <h1 class="fw-bold fs-5">WHAT IS YOUR GOAL?</h1>
       <h3 class="fw-semibold fs-6">(Select one only)</h3>
     </div>
-    <div class="border rounded-4 p-4 mx-auto shadow bg-white text-center" style="width: 300px;">
-      <div class="mx-auto" v-for="(goal, index) in goals" :key="goal.value">
-        <input
-          type="radio"
-          class="mx-2"
-          :id="goal.value"
-          :value="goal.value"
-          name="weightChoice"
-          v-model="inputGoal"
-        />
-        <label :for="goal.value" class="pe-2" :style="'color: ' + goal.color + '; font-weight: ' + fontWeight(index)">
-          {{ goal.label }}
-        </label>
+    <div class="border rounded-4 p-4 mx-auto shadow bg-white text-center col-10">
+      <div class="row">
+        <div class="col" v-for="(goal, index) in goals" :key="goal.value">
+          <label>
+            <input
+              type="radio"
+              class="card-input-element"
+              :id="goal.value"
+              :value="goal.value"
+              name="weightChoice"
+              v-model="inputGoal"
+            />
+            <div class="card card-input my-2">
+              <div style="height: 80px; width: 140px;" class="card-header text-center d-flex align-items-center justify-content-center">
+                <span :style="'color: ' + goal.color + '; font-weight: ' + fontWeight(index)">
+                  {{ goal.label }}
+                </span>
+              </div>
+            </div>
+          </label>
+        </div>
       </div>
     </div>
     <div class="mx-auto text-center">
@@ -61,7 +69,11 @@
   </div>
 </template>
 
+
+
 <script>
+import Swal from 'sweetalert2'; // Import SweetAlert2
+
 export default {
   data() {
     return {
@@ -97,9 +109,32 @@ export default {
       this.$emit('buttonAction', 'previous');
     },
     goNext() {
+      const errors = [];
       if (this.inputAge == null || this.inputWeight == null || this.inputHeight == null || this.inputGoal == null) {
-        alert('You must fill out all fields!');
-      } else {
+        errors.push('Please fill in all fields!')
+      }
+      if (isNaN(this.inputAge)) {
+        errors.push('Age must be numeric!');
+        this.inputAge = null;
+      }
+      if (isNaN(this.inputWeight)) {
+        errors.push('Weight must be numeric!');
+        this.inputWeight = null;
+      }
+      if (isNaN(this.inputHeight)) {
+        errors.push('Height must be numeric!');
+        this.inputHeight = null;
+      }
+      
+      if (errors.length > 0) {
+        const errorMessage = errors.join('\n');
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: errorMessage,
+        });
+      }
+      else {
         console.log('values : ', this.inputAge, this.inputWeight, this.inputHeight, this.inputGoal);
         this.$emit('buttonAction', 'next');
         this.$emit('addAge', this.inputAge);
@@ -111,3 +146,17 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+
+.card-input-element {
+  display: none;
+}
+.card-input:hover {
+  cursor: pointer;
+}
+
+.card-input-element:checked + .card-input {
+  box-shadow: 0 0 2px 2px #007459;
+}
+</style>
