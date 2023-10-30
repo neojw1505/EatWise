293ad1f5c360b26fac3316f3b94178e8a5290a96
DESKTOP-G5I4U2Q\JW <neojw1505@gmail.com>
2023-10-30 21:29:14 +0800

    setup AOS

--- my-vue-app/src/components/Home.vue
+++ my-vue-app/src/components/Home.vue
@@ -2,7 +2,7 @@
 <template>
   <div>
     <Navbar/>
-        <RecipeOfDay class="pt-2 my-3 mx-3 "/>
+        <RecipeOfDay data-aos="fade-up" data-aos-duration="800" data-aos-delay="200" data-aos-offset="100" class="pt-2 my-3 mx-3 "/>
         <MealPlan class=" p-4 pt-2 shadow border rounded-4 my-3 mx-3 "  />
         <NewPromoCarousel  class=" p-4 pt-2 shadow border rounded-4 my-3 mx-3 "/>
     </div>
