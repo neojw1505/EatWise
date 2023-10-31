6d03a7b2f37000919918d3b0be25736fee8e7403
wang-kaijie <99939212+wang-kaijie@users.noreply.github.com>
2023-10-31 21:45:55 +0800

    did changes to the following file in description
    remove consumption in navbar
    change the colour of saved recipe

--- my-vue-app/src/components/SavedRecipe/SavedRecipe.vue
+++ my-vue-app/src/components/SavedRecipe/SavedRecipe.vue
@@ -7,17 +7,25 @@
       <div class="mx-auto" style="max-width: 1700px">
         <div class="p-4 pt-2 shadow border rounded-4 my-3 d-flex mx-3">
           <div
-            class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6 justify-content-start g-4"
-            v-if="data"
+            class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 row-cols-xxl-6 justify-content-start g-4 mx-auto"
+            
           >
-            <!-- <FindRecipePreviewCard
-              v-for="item in data"
-              :key="item.id"
+            <div v-if="data">
+            <FindRecipePreviewCard
+              v-for="(item,recipeID) in data"
+              :key="recipeID"
               :recipe="item"
               :routerTO="item.id"
               style="text-decoration: none"
-            /> -->
+            />
+            </div>
+
+            <div v-else class="mx-auto text-center" style="width: 400px;">
+                No Saved recipe Found
+                <font-awesome-icon :icon="['fas', 'face-frown']" size="xl" />
+            </div>
           </div>
+          
         </div>
       </div>
     </div>
@@ -29,7 +37,7 @@
 export default {
   data() {
     return {
-      data: [{}],
+      data: null,
     };
   },
   methods: {
@@ -37,6 +45,9 @@ export default {
       this.data = await this.$smAPI.getUserSavedRecipes();
       console.log(this.data);
     }
+  },
+  created(){
+    this.getUserSavedRecipes();
   }
 };
 </script>
---
+++ my-vue-app/src/components/SavedRecipe/SavedRecipeCard.vue
@@ -0,0 +1,63 @@
+<template>
+    <!-- <router-link :to="routerTO"> -->
+      <div @click="selectRecipe" class="card cardStyle mx-2 px-0 my-2">
+        <img class="card-img-top mx-0" :src="recipe.image" alt="Recipe Image" />
+        <div class="card-body img-fluid">
+          <h4 class="card-title" style="font-size: large; height: 50px" >
+            {{ formattedRecipeName }}
+          </h4>
+          <p class="card-text">{{recipe.nutrition.nutrients[0].amount.toFixed(0)}} kcals</p>
+        </div>
+      </div>
+    <!-- </router-link> -->
+  </template>
+  
+  <script>
+  export default {
+    name: "SavedRecipeCard",
+    data(){return{
+      cal:null
+    }
+    },
+    //recipe is an object which consist of recipeName,image and cal.
+    //routerTo is the path which will bring the user to view the complete recipe
+  
+    props: ["recipe", "routerTO"],
+    computed: {
+      //in the event that the name of the recipe is too long, we shorten the name
+      formattedRecipeName() {
+        return this.recipe.title.length > 30
+          ? this.recipe.title.slice(0, 27) + "..."
+          : this.recipe.title;
+      },
+    },
+    methods: {
+      async getSelectedRecipeNutritions() {
+        let recipeObj= await this.$spoonAPI.getSelectedRecipeNutritions(this.recipe.id);
+        this.cal=recipeObj.calories;
+      },
+      async selectRecipe() {
+        // Use router to navigate to the SelectedRecipe component with the recipe id as a parameter
+        this.$router.push({ path: '/find-recipes/SelectedRecipeCard/:id', qeury:{id: "1234" }})
+      },
+    },
+    
+  };
+  </script>
+  
+  <style scoped>
+  .cardStyle {
+    border: #FFB18D solid 1px;
+    border-bottom-left-radius: 30px;
+    width: 200px;
+    height: 250px;
+    background-image: linear-gradient(to bottom right, #ff5100, #fac400);
+    color: black;
+    transition: transform 0.3s;
+  }
+  .cardStyle:hover {
+    transform: scale(1.05); /* Scale up by 10% on hover */
+    cursor: pointer;
+  }
+  </style>
+  
