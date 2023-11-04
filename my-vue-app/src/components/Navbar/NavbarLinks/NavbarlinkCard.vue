371257587f7c17c923292e2fe1c63bc7dcb624bb
wang-kaijie <99939212+wang-kaijie@users.noreply.github.com>
2023-11-05 02:21:35 +0800

    change the navbar highlight when at that page or hover

--- my-vue-app/src/components/Navbar/NavbarLinks/NavbarlinkCard.vue
+++ my-vue-app/src/components/Navbar/NavbarLinks/NavbarlinkCard.vue
@@ -2,7 +2,7 @@
     <router-link class="nav-link active py-0 d-flex mx-auto " aria-current="page" :to="eachLink.routeTo">
     <li :class="highlighLink(eachLink)" >
         <font-awesome-icon :icon="icon" size="lg" /> <!-- Use icon prop to set the Font Awesome icon -->
-        <div class="fw-semibold" v-html="`&nbsp;&nbsp;${eachLink.linkName}&nbsp;&nbsp;`"></div> 
+        <div class="fw-semibold text-black" v-html="`&nbsp;&nbsp;${eachLink.linkName}&nbsp;&nbsp;`"></div> 
     </li>
     </router-link>
 </template>
