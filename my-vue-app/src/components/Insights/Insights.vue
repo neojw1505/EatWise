5132b056118aa6d06dfba1670af1ac9af67ed489
lemuelfwl <lemuelfwl@gmail.com>
2023-11-08 20:12:46 +0800

    added banner for insights page
    added banner for insights page

--- my-vue-app/src/components/Insights/Insights.vue
+++ my-vue-app/src/components/Insights/Insights.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
       <navbar />
+      <div class="background-img">
+      <!-- Add background Image -->
+      <h2 class="m-0 text-white fw-bold d-flex " style="padding-top:200px; padding-left:40px; font-size:60px">Insights</h2>
+    </div>
       <div class="row my-2 mx-2" style="padding-top:100px">
           <MacroNutrientsChart class="col-md-6 col-12 me-2  border shadow rounded-4" />
           <ActionableInsights class="col border shadow rounded-4"/>
@@ -12,4 +16,15 @@
     </div>
   </template>
   
-  
+<style scoped>
+
+.background-img{
+  background-image: url(../../components/homepageAsset/insights.jpg);
+  width: 100%;
+  height: 280px;
+  object-fit: fill;   
+  background-size: 100%;
+  background-repeat: no-repeat;
+}
+
+</style>
