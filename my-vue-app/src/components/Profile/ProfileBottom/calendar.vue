a0bbf775d16f936c59e9ab898aa8fe8477412481
DESKTOP-G5I4U2Q\JW <neojw1505@gmail.com>
2023-11-09 03:37:02 +0800

    fix minor css

--- my-vue-app/src/components/Profile/ProfileBottom/calendar.vue
+++ my-vue-app/src/components/Profile/ProfileBottom/calendar.vue
@@ -1,84 +1,130 @@
 <template>
-    <div class="m-0 text-align-center ">
-      <div class="d-flex justify-content-between align-items-center p-3 rounded-4 rounded-bottom-0 " style="background-color: transparent; color:white">
-        <button class="btn btn-secondary bg-transparent-subtle border-transparent" @click="previousMonth">&lt;</button>
-        <h4>{{ formattedDate }}</h4>
-        <button class="btn btn-secondary bg-transparent-subtle border-transparent" @click="nextMonth">&gt;</button>
-      </div>
-      <div class="d-flex" style="background-color: transparent;">
-        <div v-for="day in days" class="calendar-day text-light" :key="day">{{ day }}</div>
-      </div>
-      <div class="calendar-grid">
-        <div  v-for="date in calendarDates" :key="date" class="calendar-cell text-light" @click="selectDate(date)"
-        :class="{ 'selected-date': isDateSelected(date) }">
-          {{ date }}
-        </div>
+  <div class="m-0 text-align-center">
+    <div
+      class="d-flex justify-content-between align-items-center p-3 rounded-4 rounded-bottom-0"
+      style="background-color: transparent; color: white"
+    >
+      <button
+        class="btn bg-transparent-subtle border-transparent"
+        @click="previousMonth"
+      >
+        &lt;
+      </button>
+      <h4>{{ formattedDate }}</h4>
+      <button
+        class="btn bg-transparent-subtle border-transparent"
+        @click="nextMonth"
+      >
+        &gt;
+      </button>
+    </div>
+    <div class="d-flex" style="background-color: transparent">
+      <div v-for="day in days" class="calendar-day text-light" :key="day">
+        {{ day }}
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        today: new Date(),
-        currentMonth: new Date(),
-        selectedDateOnCalendar: new Date(),
-      };
-    },
-    computed: {
-      days() {
-        return ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-      },
-      calendarDates() {
-        const firstDay = new Date(this.currentMonth.getFullYear(), this.currentMonth.getMonth(), 1);
-        const lastDay = new Date(this.currentMonth.getFullYear(), this.currentMonth.getMonth() + 1, 0);
-        const daysInMonth = lastDay.getDate();
-        const firstDayOfWeek = firstDay.getDay();
-  
-        const dates = [];
-        for (let i = 1; i <= daysInMonth; i++) {
-          dates.push(i);
-        }
-  
-        const leadingBlanks = new Array(firstDayOfWeek).fill(null);
-        const trailingBlanks = new Array(6 - ((dates.length + firstDayOfWeek - 1) % 7)).fill(null);
-  
-        return [...leadingBlanks, ...dates, ...trailingBlanks];
-      },
-      formattedDate(){
-        let date=this.selectedDateOnCalendar.getDate();
-        let month=this.selectedDateOnCalendar.getMonth()+1;
-        let year=this.selectedDateOnCalendar.getFullYear();
+    <div class="calendar-grid">
+      <div
+        v-for="date in calendarDates"
+        :key="date"
+        class="calendar-cell text-light"
+        @click="selectDate(date)"
+        :class="{ 'selected-date': isDateSelected(date) }"
+      >
+        {{ date }}
+      </div>
+    </div>
+  </div>
+</template>
 
-        // console.log(tempDate)
-        let monthObj={
-          '1':"January",
-          '2':"February",
-          '3':"March",
-          '4':"April",
-          '5':"May",
-          '6':"June",
-          '7':"July",
-          '8':"August",
-          '9':"September",
-          '10':"October",
-          '11':"November",
-          '12':"December",
-        };
-        return date+" "+monthObj[month]+" "+year;
-      }
+<script>
+export default {
+  data() {
+    return {
+      today: new Date(),
+      currentMonth: new Date(),
+      selectedDateOnCalendar: new Date(),
+    };
+  },
+  computed: {
+    days() {
+      return ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
     },
-    methods: {
-      previousMonth() {
-        this.currentMonth = new Date(this.currentMonth.getFullYear(), this.currentMonth.getMonth() - 1, 1);
-        this.selectedDateOnCalendar=new Date(this.currentMonth.getFullYear(), this.currentMonth.getMonth(), 1)
-      },
-      nextMonth() {
-        this.currentMonth = new Date(this.currentMonth.getFullYear(), this.currentMonth.getMonth() + 1, 1);
-        this.selectedDateOnCalendar=new Date(this.currentMonth.getFullYear(), this.currentMonth.getMonth(), 1)
-      },
-      selectDate(date) {
+    calendarDates() {
+      const firstDay = new Date(
+        this.currentMonth.getFullYear(),
+        this.currentMonth.getMonth(),
+        1
+      );
+      const lastDay = new Date(
+        this.currentMonth.getFullYear(),
+        this.currentMonth.getMonth() + 1,
+        0
+      );
+      const daysInMonth = lastDay.getDate();
+      const firstDayOfWeek = firstDay.getDay();
+
+      const dates = [];
+      for (let i = 1; i <= daysInMonth; i++) {
+        dates.push(i);
+      }
+
+      const leadingBlanks = new Array(firstDayOfWeek).fill(null);
+      const trailingBlanks = new Array(
+        6 - ((dates.length + firstDayOfWeek - 1) % 7)
+      ).fill(null);
+
+      return [...leadingBlanks, ...dates, ...trailingBlanks];
+    },
+    formattedDate() {
+      let date = this.selectedDateOnCalendar.getDate();
+      let month = this.selectedDateOnCalendar.getMonth() + 1;
+      let year = this.selectedDateOnCalendar.getFullYear();
+
+      // console.log(tempDate)
+      let monthObj = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December",
+      };
+      return date + " " + monthObj[month] + " " + year;
+    },
+  },
+  methods: {
+    previousMonth() {
+      this.currentMonth = new Date(
+        this.currentMonth.getFullYear(),
+        this.currentMonth.getMonth() - 1,
+        1
+      );
+      this.selectedDateOnCalendar = new Date(
+        this.currentMonth.getFullYear(),
+        this.currentMonth.getMonth(),
+        1
+      );
+    },
+    nextMonth() {
+      this.currentMonth = new Date(
+        this.currentMonth.getFullYear(),
+        this.currentMonth.getMonth() + 1,
+        1
+      );
+      this.selectedDateOnCalendar = new Date(
+        this.currentMonth.getFullYear(),
+        this.currentMonth.getMonth(),
+        1
+      );
+    },
+    selectDate(date) {
       // When a calendar cell is clicked, update the selected date.
       this.selectedDateOnCalendar = new Date(
         this.currentMonth.getFullYear(),
@@ -86,7 +132,7 @@
         date
       );
       // console.log(this.selectedDateOnCalendar)
-      this.$emit('getCalendarDate',this.selectedDateOnCalendar)
+      this.$emit("getCalendarDate", this.selectedDateOnCalendar);
     },
     isDateSelected(date) {
       // Check if the date matches the selected date.
@@ -95,43 +141,48 @@
       }
       return (
         date === this.selectedDateOnCalendar.getDate() &&
-        this.currentMonth.getMonth() === this.selectedDateOnCalendar.getMonth() &&
-        this.currentMonth.getFullYear() === this.selectedDateOnCalendar.getFullYear()
+        this.currentMonth.getMonth() ===
+          this.selectedDateOnCalendar.getMonth() &&
+        this.currentMonth.getFullYear() ===
+          this.selectedDateOnCalendar.getFullYear()
       );
     },
-    },
-  };
-  </script>
-  
-  <style scoped>
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  background-color:#e0c580;
+  color: #FFF;
+}
+
 .selected-date {
-  background-color: rgb(114, 105, 105);
+  background-color: #e0c580;
   color: white;
   border: 2px solid black;
 }
-  
-  .calendar-day {
-    flex: 1;
-    padding: 10px;
-  }
-  
-  .calendar-grid {
-    display: grid;
-    grid-template-columns: repeat(7, 1fr);
-  }
-  
-  .calendar-cell {
-    padding: 10px;
-    min-height: 50px;
-    text-align: center;
-    border-radius: 50px;
 
-  }
+.calendar-day {
+  flex: 1;
+  padding: 10px;
+}
 
-  .calendar-cell:hover {
-    cursor: pointer;
-    transform: scale(1.05); 
-    transition: transform 0.3s; 
-  }
-  </style>
-  
+.calendar-grid {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+}
+
+.calendar-cell {
+  padding: 10px;
+  min-height: 50px;
+  text-align: center;
+  border-radius: 50px;
+}
+
+.calendar-cell:hover {
+  cursor: pointer;
+  transform: scale(1.05);
+  transition: transform 0.3s;
+}
+</style>
